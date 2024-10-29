da9ef8a564253c1e73d99d4560c325dc4836a5b7
Rawan1312 <3kjxx.58@gmail.com>
2024-10-29 14:35:20 +0300

    adding db

--- Migrations/20241029113336_InitalCreate.Designer.cs
+++ Migrations/20241029113336_InitalCreate.Designer.cs
@@ -11,8 +11,8 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDBContext))]
-    [Migration("20241006200022_edit3")]
-    partial class edit3
+    [Migration("20241029113336_InitalCreate")]
+    partial class InitalCreate
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
