b431114f529dc2a7537597cfd44ab1aae19ab85a
Rawan1312 <3kjxx.58@gmail.com>
2024-11-12 01:40:33 +0300

    update service1

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,71 +1,39 @@
-// using System;
-// using System.Collections.Generic;
-// using System.IdentityModel.Tokens.Jwt;
-// using System.Linq;
-// using System.Security.Claims;
-// using System.Text;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.IdentityModel.Tokens;
-
-
-
-// [ApiController]
-//     [Route("/api/v1/auth")]
-//     public class AuthController : ControllerBase
-//     {
-//         private readonly AuthService _authService;
-//         public AuthController(AuthService authService)
-//         {
-//             _authService = authService;
-//         }
-
-//         [HttpPost("register")]
-//         public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
-//         {
-//             var result = await _authService.RegisterUserService(userRegisterDto);
-//             return Created("", result);
-//         }
-
-//         [HttpPost("login")]
-//         public async Task<IActionResult> Login(UserLoginDto userLoginDto)
-//         {
-//             var token = await _authService.LoginService(userLoginDto);
-//             return Created("", new { Token = token });
-//         }
-
-        
-//     }
-using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+
 
 [ApiController]
-[Route("/api/v1/auth")]
-public class AuthController : ControllerBase
-{
-    private readonly AuthService _authService;
-
-    public AuthController(AuthService authService)
+    [Route("/api/v1/auth")]
+    public class AuthController : ControllerBase
     {
-        _authService = authService;
-    }
-
-    [HttpPost("register")]
-    public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
-    {
-        var result = await _authService.RegisterUserService(userRegisterDto);
-        return Ok(new { message = result });
-    }
-
-    [HttpPost("login")]
-    public async Task<IActionResult> Login(UserLoginDto userLoginDto)
-    {
-        var token = await _authService.LoginService(userLoginDto);
-        if (token == "Invalid email or password")
+        private readonly AuthService _authService;
+        public AuthController(AuthService authService)
         {
-            return Unauthorized(new { message = token });
+            _authService = authService;
         }
-        return Ok(new { token = token });
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var result = await _authService.RegisterUserService(userRegisterDto);
+            return Created("", result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserLoginDto userLoginDto)
+        {
+            var token = await _authService.LoginService(userLoginDto);
+            return Created("", new { Token = token });
+        }
+
+        
     }
-}
--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,162 +1,89 @@
-// using System;
-// using System.Collections.Generic;
-// using System.IdentityModel.Tokens.Jwt;
-// using System.Linq;
-// using System.Security.Claims;
-// using System.Text;
-// using System.Threading.Tasks;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.IdentityModel.Tokens;
-
-// public class AuthService {
-//  private readonly AppDBContext _appDbContext;
-//         private readonly IConfiguration _configuration;
-
-//         public AuthService(AppDBContext appDbContext, IConfiguration configuration)
-//         {
-//             _appDbContext = appDbContext;
-//             _configuration = configuration; }
-            
-//             public async Task<string> RegisterUserService(UserRegisterDto userRegisterDto)
-//         {
-//             var user = new User
-//             {
-//                 UserId = Guid.NewGuid(),
-//                 Name = userRegisterDto.Name,
-//                 Email = userRegisterDto.Email,
-//                 Password = BCrypt.Net.BCrypt.HashPassword(
-//                 userRegisterDto.Password),
-//                // Address = userRegisterDto.Address ?? string.Empty,
-                
-//             };
-//              await _appDbContext.Users.AddAsync(user);
-//             await _appDbContext.SaveChangesAsync();
-
-//             return "user is created successfully"; }
-            
-//              public async Task<string> LoginService(UserLoginDto userLoginDto)
-//         {
-//             var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == userLoginDto.Email);
-//             if (user == null || !BCrypt.Net.BCrypt.Verify(userLoginDto.Password, user.Password))
-//             {
-//                 return "Email/Password is incorrect";
-//             }
-
-//             var token = GenerateJwtToken(user);
-
-//             return token;
-//         }
-
-//         public string GenerateJwtToken(User user)
-//         {
-//             var tokenHandler = new JwtSecurityTokenHandler(); // Handler responsible for creating and validating JWTs. It handles the token's lifecycle.
-
-//             // Convert the secret key from the configuration into a byte array.
-//             var jwtKey =  Environment.GetEnvironmentVariable("Jwt__Key") ?? throw new InvalidOperationException("JWT Key is missing in configuration.");
-
-//             var key = Encoding.ASCII.GetBytes(jwtKey);
-
-
-//             // settings for token: Describe the token using SecurityTokenDescriptor.
-//             var tokenDescriptor = new SecurityTokenDescriptor // SetUP
-//             {
-
-//                 Subject = new ClaimsIdentity(new[]
-//                 {
-//                 // optional claims
-//                 new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()), // Often used to store a user ID, which is critical for identifying the user within your system.
-//                 new Claim(ClaimTypes.Name, user.Name), // User's name.
-//                 new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User") // User's role, determining access level.
-//             }),
-//                 Expires = DateTime.UtcNow.AddMinutes(5), // Set the token to expire in 1 minute from creation.
-//                 // Expires = DateTime.UtcNow.AddDays(2), // Set the token to expire in 2 hours from creation.
-//                 // Expires = DateTime.UtcNow.AddHours(2),
-
-//                 // The signing credentials contain the security key and the algorithm used for signature validation.
-//                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-
-//                 // optional
-//                 Issuer = Environment.GetEnvironmentVariable("Jwt__Issuer"), // "iss" (issuer) claim: The issuer of the token.
-//                 Audience = Environment.GetEnvironmentVariable("Jwt__Audience") // "aud" (audience) claim: Intended recipient of the token.
-//             };
-
-//             // Create the token based on the descriptor.
-//             var token = tokenHandler.CreateToken(tokenDescriptor);
-
-//             // Serialize the token to a JWT string.
-//             return tokenHandler.WriteToken(token);
-//         }}
-
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using BCrypt.Net;
 
-public class AuthService
-{
-    private readonly AppDBContext _appDbContext;
-    private readonly IConfiguration _configuration;
+public class AuthService {
+ private readonly AppDBContext _appDbContext;
+        private readonly IConfiguration _configuration;
 
-    public AuthService(AppDBContext appDbContext, IConfiguration configuration)
-    {
-        _appDbContext = appDbContext;
-        _configuration = configuration;
-    }
-
-    public async Task<string> RegisterUserService(UserRegisterDto userRegisterDto)
-    {
-        var user = new User
+        public AuthService(AppDBContext appDbContext, IConfiguration configuration)
         {
-            UserId = Guid.NewGuid(),
-            Name = userRegisterDto.Name,
-            Email = userRegisterDto.Email,
-            Password = BCrypt.Net.BCrypt.HashPassword(userRegisterDto.Password),
-        };
-
-        await _appDbContext.Users.AddAsync(user);
-        await _appDbContext.SaveChangesAsync();
-
-        return "User registered successfully";
-    }
-
-    public async Task<string> LoginService(UserLoginDto userLoginDto)
-    {
-        var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == userLoginDto.Email);
-        if (user == null || !BCrypt.Net.BCrypt.Verify(userLoginDto.Password, user.Password))
+            _appDbContext = appDbContext;
+            _configuration = configuration; }
+            
+            public async Task<string> RegisterUserService(UserRegisterDto userRegisterDto)
         {
-            return "Invalid email or password";
+            var user = new User
+            {
+                UserId = Guid.NewGuid(),
+                Name = userRegisterDto.Name,
+                Email = userRegisterDto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(
+                userRegisterDto.Password),
+               // Address = userRegisterDto.Address ?? string.Empty,
+                
+            };
+             await _appDbContext.Users.AddAsync(user);
+            await _appDbContext.SaveChangesAsync();
+
+            return "user is created successfully"; }
+            
+             public async Task<string> LoginService(UserLoginDto userLoginDto)
+        {
+            var user = await _appDbContext.Users.FirstOrDefaultAsync(u => u.Email == userLoginDto.Email);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(userLoginDto.Password, user.Password))
+            {
+                return "Email/Password is incorrect";
+            }
+
+            var token = GenerateJwtToken(user);
+
+            return token;
         }
 
-        var token = GenerateJwtToken(user);
-        return token;
-    }
-
-    public string GenerateJwtToken(User user)
-    {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var jwtKey = Environment.GetEnvironmentVariable("Jwt__Key") ?? throw new InvalidOperationException("JWT Key is missing");
-        var key = Encoding.ASCII.GetBytes(jwtKey);
-
-        var tokenDescriptor = new SecurityTokenDescriptor
+        public string GenerateJwtToken(User user)
         {
-            Subject = new ClaimsIdentity(new[] {
-                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
-                new Claim(ClaimTypes.Name, user.Name),
-                new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User")
-            }),
-            Expires = DateTime.UtcNow.AddMinutes(60),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-            Issuer = Environment.GetEnvironmentVariable("Jwt__Issuer"),
-            Audience = Environment.GetEnvironmentVariable("Jwt__Audience")
-        };
+            var tokenHandler = new JwtSecurityTokenHandler(); // Handler responsible for creating and validating JWTs. It handles the token's lifecycle.
 
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        return tokenHandler.WriteToken(token);
-    }
-}
+            // Convert the secret key from the configuration into a byte array.
+            var jwtKey =  Environment.GetEnvironmentVariable("Jwt__Key") ?? throw new InvalidOperationException("JWT Key is missing in configuration.");
+
+            var key = Encoding.ASCII.GetBytes(jwtKey);
+
+
+            // settings for token: Describe the token using SecurityTokenDescriptor.
+            var tokenDescriptor = new SecurityTokenDescriptor // SetUP
+            {
+
+                Subject = new ClaimsIdentity(new[]
+                {
+                // optional claims
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()), // Often used to store a user ID, which is critical for identifying the user within your system.
+                new Claim(ClaimTypes.Name, user.Name), // User's name.
+                new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User") // User's role, determining access level.
+            }),
+                Expires = DateTime.UtcNow.AddMinutes(5), // Set the token to expire in 1 minute from creation.
+                // Expires = DateTime.UtcNow.AddDays(2), // Set the token to expire in 2 hours from creation.
+                // Expires = DateTime.UtcNow.AddHours(2),
+
+                // The signing credentials contain the security key and the algorithm used for signature validation.
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+
+                // optional
+                Issuer = Environment.GetEnvironmentVariable("Jwt__Issuer"), // "iss" (issuer) claim: The issuer of the token.
+                Audience = Environment.GetEnvironmentVariable("Jwt__Audience") // "aud" (audience) claim: Intended recipient of the token.
+            };
+
+            // Create the token based on the descriptor.
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            // Serialize the token to a JWT string.
+            return tokenHandler.WriteToken(token);
+        }}
